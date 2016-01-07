3fe0a9283528c50579bdb97775ed95f6693d7e34
ArsenShnurkov <Arsen.Shnurkov@gmail.com>
2016-01-07 22:19:31 +0300

    unit test for SHOW TABLES

--- src/nunit-adonet/Test.cs
+++ src/nunit-adonet/Test.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Configuration;
 using System.Data.Common;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace nunitadonet
 {
@@ -22,6 +24,49 @@ namespace nunitadonet
 			var providerName = cs.ProviderName;
 			var factory = DbProviderFactories.GetFactory(providerName); 			
 			Assert.IsNotNull (factory);
+		}
+
+		[Test]
+		public void Test_RunSimpleDbCommand()
+		{
+			var cs = ConfigurationManager.ConnectionStrings["MyConnectionString"];
+			var providerName = cs.ProviderName;
+			var factory = DbProviderFactories.GetFactory(providerName); 			
+			Assert.IsNotNull (factory);
+			var connectionString = cs.ConnectionString;
+			using (var con = factory.CreateConnection ())
+			{
+				Assert.IsNotNull (con);
+				con.ConnectionString = connectionString;
+				con.Open ();
+				using (var cmd = con.CreateCommand())
+				{
+					Assert.IsNotNull (cmd);
+					cmd.CommandText = "SHOW TABLES;";
+					using (var reader = cmd.ExecuteReader())
+					{
+						int columnsCount = -1;
+						while (reader.Read())
+						{
+							columnsCount = reader.FieldCount;
+							var fields = new List<string>(columnsCount);
+							for (int i = 0; i < columnsCount; ++i)
+							{
+								var o = reader[i];
+								var t = reader.GetFieldType(i);
+								if (t != typeof(string))
+								{
+									o = Convert.ChangeType(o, typeof(string), Thread.CurrentThread.CurrentCulture);
+								}
+								string str = (string)o;
+								fields.Add(str);
+							}
+						}
+						reader.Close();
+					}
+					con.Close();
+				}
+			}
 		}
 	}
 }
