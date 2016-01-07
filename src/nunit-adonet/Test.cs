0789cdc061a0e54aef84186aa58d46d38d14e5ee
ArsenShnurkov <Arsen.Shnurkov@gmail.com>
2016-01-07 21:55:53 +0300

    tests for ADO .NET

---
+++ src/nunit-adonet/Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Configuration;
+using System.Data.Common;
+
+namespace nunitadonet
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test]
+		public void VerifyAppDomainHasConfigurationSettings()
+		{
+			string value = ConfigurationManager.AppSettings["MyTestConfig"];
+			Assert.IsFalse(String.IsNullOrEmpty(value), "No App.Config found.");
+		}
+
+		[Test ()]
+		public void Test_DbProviderFactories_GetFactory()
+		{
+			var cs = ConfigurationManager.ConnectionStrings["MyConnectionString"];
+			var providerName = cs.ProviderName;
+			var factory = DbProviderFactories.GetFactory(providerName); 			
+			Assert.IsNotNull (factory);
+		}
+	}
+}
